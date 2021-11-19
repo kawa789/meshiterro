cb44776e47e49416ca0db8818a7c472c8662a1b6
EC2 Default User <ec2-user@ip-172-31-92-166.ec2.internal>
2021-11-19 18:54:25 +0000

    [Add]画像削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root 'homes#top'
   devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  root 'homes#top'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
 end
 
 
