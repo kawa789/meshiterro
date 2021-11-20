6e1121d7001915d69e9f2d9407122460e57f7c3b
EC2 Default User <ec2-user@ip-172-31-92-166.ec2.internal>
2021-11-20 09:34:02 +0000

    [Add]マイページ

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -12,7 +12,7 @@ class PostImagesController < ApplicationController
   end
   
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page]).reverse_order
   end
   
   def show
@@ -33,3 +33,4 @@ class PostImagesController < ApplicationController
   end
   
 end
+
